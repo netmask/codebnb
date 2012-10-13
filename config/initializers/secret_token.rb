60d74dc44d90cac696809630ae348b269703e2af
Jonathan Garay <jonathan@devmask.net>
2012-10-12 17:00:30 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codebnb::Application.config.secret_token = '9cea4acd96b5c23b54869c5813293698b4f1d29eda1aab3100ad9504a52a07683745285a771801332cf24975a287720e1ea3caf6404c1e40b9d6e0ca8b7d05c0'
