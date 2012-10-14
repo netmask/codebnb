b437383187eb9c6e2916d1bdd8274da9069bf2d2
Jonathan Garay <jonathan@devmask.net>
2012-10-14 09:41:55 -0700

    HOTFIX: Typo on reservation controller

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,13 +5,11 @@ class Reservation < ActiveRecord::Base
 
   attr_accessible :status
 
-  validates_uniqueness_of :user_id
-
   def self.requests(venue_id, user_id)
     joins(:period => :venue).where(:venues =>{user_id: user_id, id: venue_id})
   end
 
   def self.request(venue_id, hoster_id, requester_id)
-    requests_for(venue_id, requester_id).where(:user_id: hoster_id)
+    requests(venue_id, requester_id).where(user_id: hoster_id)
   end
 end
